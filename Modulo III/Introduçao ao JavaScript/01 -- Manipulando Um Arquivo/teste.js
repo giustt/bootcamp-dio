f67c8abbd49a2e5816d163e306a56a34f86a057f
giustt <gustavodecassio00@gmail.com>
2022-05-24 18:10:43 -0300

    Adding Module III

---
+++ Modulo III/Introduçao ao JavaScript/01 -- Manipulando Um Arquivo/teste.js
@@ -0,0 +1,14 @@
+// var y = 0;
+// var y = 0;
+// var x =1;
+// cntrl + barra comenta
+
+// var preco = 2;
+
+
+var desconto = 0.2;
+const PRECO = 2;
+
+
+PRECO = PRECO - desconto;
+var total = PRECO - desconto;
