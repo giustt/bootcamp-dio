89cf1c6705fd525b5b06e2e39be12bd070531701
giustt <gustavodecassio00@gmail.com>
2022-06-06 23:11:12 -0300

    Ending the Day

--- Modulo III/03 -- Variaveis e Tipos/teste.js
+++ Modulo III/03 -- Variaveis e Tipos/teste.js
@@ -1,2 +1,23 @@
 // Hoisting		-> Atribuir o valor a uma variavel antes de declarar ela (somente para var)
 // let e const  -> escopo de bloco
+// Constantes sao declaradas em SNAKE_UPPER_CASE  Escopo de Bloco
+
+numberOne = 1;
+console.log(numberOne + 2)
+var numberOne
+
+
+
+var firstName = 'Gustavo';
+var lastName = 'Perez';
+
+if(firstName === 'Gustavo'){
+    let lastName = 'Rodrigues'
+    var firstName = 'Giustt';
+    lastName = 'Souza';
+
+    console.log(lastName);
+}
+
+console.log(firstName, lastName);
+
