d6de09b09310e4db9efeca4a42d58fea9c86c072
giustt <gustavodecassio00@gmail.com>
2022-07-16 18:15:15 -0300

    Adding URL Shortener

---
+++ Modulo VI -- NodeJs/Encurtador De Url/README.md
@@ -0,0 +1,10 @@
+# Encurtador de URLs
+
+Projeto criado junto da DIO para construção de um Encurtador de URL.
+
+## Para Rodar o projeto:
+
+- `npm install`
+- `npm run build`
+- `npm run dev`
+
---
+++ Modulo VI -- NodeJs/Encurtador De Url/src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:3000',
+	MONGO_CONNECTION:
+		'mongodb+srv://giustt:<password>@cluster0.z29wytl.mongodb.net/?retryWrites=true&w=majority',
+}
